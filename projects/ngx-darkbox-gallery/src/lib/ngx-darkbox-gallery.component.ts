947086592b36c09d438c0b1ab1367f2c2131cb78
bjoernboehnke <git@bjoern-boehnke.de>
2021-02-27 16:19:25 +0100

    First running demo app with linked lib

--- projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.component.ts
+++ projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-ngx-darkbox-gallery',
+  selector: 'darkbox-gallery',
   template: `
     <p>
-      ngx-darkbox-gallery works!
+      ngx-darkbox-gallery works! TEST
     </p>
   `,
   styles: [
