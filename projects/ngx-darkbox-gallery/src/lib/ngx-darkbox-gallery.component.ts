07b6d00c435784936fb067889eaf45da0e3bfe42
bjoernboehnke <git@bjoern-boehnke.de>
2021-02-28 17:51:50 +0100

    Included darkbox in gallery

--- projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.component.ts
+++ projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { DefaultConfiguration } from './config/configuration.default';
 import { Configuration } from './model/configuration';
+import { LoopDirection } from './model/darkbox-configuration';
 import { Image } from './model/image';
 
 @Component({
@@ -13,6 +14,7 @@ export class NgxDarkboxGalleryComponent implements OnInit, OnChanges {
   @Input()
   images: Image[] = [];
   imageCount: number;
+  currentImageIndex: number;
 
   @Input()
   configuration: Configuration;
@@ -21,6 +23,18 @@ export class NgxDarkboxGalleryComponent implements OnInit, OnChanges {
 
   @Output()
   imageClicked = new EventEmitter<Image>();
+
+  @Output()
+  darkboxClosed = new EventEmitter<boolean>();
+
+  @Output()
+  darkboxNext = new EventEmitter<boolean>();
+
+  @Output()
+  darkboxPrev = new EventEmitter<boolean>();
+
+  @Output()
+  darkboxImageLoaded = new EventEmitter<Image>();
 
   constructor() { }
 
@@ -41,6 +55,69 @@ export class NgxDarkboxGalleryComponent implements OnInit, OnChanges {
     this.imageCount = this.effectiveConfiguration.gridConfiguration.initialBatchSize;
   }
 
+  onImageClicked(image: Image): void {
+    this.currentImageIndex = this.images.indexOf(image);
+    this.imageClicked.emit(image);
+  }
+
+  onDarkboxClose(): void {
+    this.currentImageIndex = null;
+    this.darkboxClosed.emit(true);
+  }
+
+  onDarkboxNextImage(): void {
+    this.currentImageIndex = this.calculateValidImageIndex(true);
+    this.darkboxNext.emit(true);
+  }
+
+  onDarkboxPrevImage(): void {
+    this.currentImageIndex = this.calculateValidImageIndex(false);
+    this.darkboxPrev.emit(true);
+  }
+
+  onDarkboxImageLoaded(image: Image): void {
+    this.darkboxImageLoaded.emit(image);
+  }
+
+  isNextImageAvailable(): boolean {
+    const loopDirection = this.effectiveConfiguration.darkboxConfiguration.loopDirection;
+    return this.currentImageIndex + 1 < this.images.length || loopDirection == LoopDirection.FORWARD || loopDirection == LoopDirection.BOTH;
+  }
+
+  isPrevImageAvailable(): boolean {
+    const loopDirection = this.effectiveConfiguration.darkboxConfiguration.loopDirection;
+    return this.currentImageIndex - 1 >= 0 || loopDirection == LoopDirection.BACKWARD || loopDirection == LoopDirection.BOTH;
+  }
+
+  /**
+   * Calculates the next valid image index based on the currentImageIndex and the images array
+   * @param increase true if the index should be increased, false otherwise
+   * @return the new index
+   */
+  private calculateValidImageIndex(increase: boolean): number {
+    const loopDirection = this.effectiveConfiguration.darkboxConfiguration.loopDirection;
+
+    const addend = increase ? 1 : -1;
+    const targetIndex = this.currentImageIndex + addend;
+    const maxImageIndex = this.images.length -1;
+
+    if (targetIndex >= 0 && targetIndex <= maxImageIndex) {
+      return this.currentImageIndex + addend;
+    }
+
+    if (loopDirection != LoopDirection.NONE) {
+      if (targetIndex > maxImageIndex && (loopDirection == LoopDirection.FORWARD || loopDirection == LoopDirection.BOTH)) {
+        return 0;
+      }
+
+      if (targetIndex < 0 && (loopDirection == LoopDirection.BACKWARD || loopDirection ==  LoopDirection.BOTH)) {
+        return maxImageIndex;
+      }
+    }
+
+    return this.currentImageIndex;
+  }
+
   /**
    * Calculates the correct initial number of images based on the screen resolution
    */
@@ -52,7 +129,6 @@ export class NgxDarkboxGalleryComponent implements OnInit, OnChanges {
     const factor = scalingFactors.find(factor => windowWidth >= factor.pxWidth);
 
     this.effectiveConfiguration.gridConfiguration.initialBatchSize *= factor ? factor.scalingFactor : 1;
-    console.log(this.effectiveConfiguration);
   }
 
 }
