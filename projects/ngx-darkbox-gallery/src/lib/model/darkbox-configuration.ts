15f6125faf8749d3c58d4712da7a78837b0ff17c
bjoernboehnke <git@bjoern-boehnke.de>
2021-02-28 17:49:31 +0100

    Added imageLoop config

--- projects/ngx-darkbox-gallery/src/lib/model/darkbox-configuration.ts
+++ projects/ngx-darkbox-gallery/src/lib/model/darkbox-configuration.ts
@@ -1,3 +1,17 @@
 export interface DarkboxConfiguration {
+  /**
+   * Sets the direction the darkbox will allow looping through the images
+   * FORWARD: Only after the last image the first will be displayed again
+   * BACKWARD: Only before the first image the last will be displayed again
+   * BOTH: The first image will be displayed after the last and vise versa
+   * NONE: There is a fixed end in both directions
+   */
+  loopDirection: LoopDirection;
+}
 
+export enum LoopDirection {
+  FORWARD = 'FORWARD',
+  BACKWARD = 'BACKWARD',
+  BOTH = 'BOTH',
+  NONE = 'NONE'
 }
