2b07151419da8c6e5c1f6dd24ea958a8d7ba94d4
bjoernboehnke <git@bjoern-boehnke.de>
2021-02-28 19:14:17 +0100

    Added keyboard navigation to darkbox

--- projects/ngx-darkbox-gallery/src/lib/components/darkbox/darkbox.component.ts
+++ projects/ngx-darkbox-gallery/src/lib/components/darkbox/darkbox.component.ts
@@ -1,6 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { DarkboxConfiguration } from '../../model/darkbox-configuration';
 import { Image } from '../../model/image';
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37,
+  ESCAPE = 27
+}
 
 @Component({
   selector: 'darkbox',
@@ -58,5 +64,27 @@ export class DarkboxComponent implements OnInit {
 
   onImageLoaded(): void {
     this.imageLoaded.emit(this.image);
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  onKeyUp(event: KeyboardEvent) {
+    let code: string | number;
+    if (event.key !== undefined) {
+      code = event.key;
+    } else if (event.keyCode !== undefined) {
+      code = event.keyCode;
+    }
+
+    if (this.hasNext && (code === 'ArrowRight' || code === KEY_CODE.RIGHT_ARROW)) {
+      this.onNext();
+    }
+
+    if (this.hasPrev && (code === 'ArrowLeft' || code === KEY_CODE.LEFT_ARROW)) {
+      this.onPrev();
+    }
+
+    if (code === 'Escape' || code === KEY_CODE.ESCAPE) {
+      this.onClose();
+    }
   }
 }
