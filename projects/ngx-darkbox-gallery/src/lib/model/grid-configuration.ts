766601ec85f278c04f83394d286d8c52ded5f624
Henning Ramberger <kontakt@hramberger.de>
2021-03-31 19:41:31 +0200

    Made config values optional

--- projects/ngx-darkbox-gallery/src/lib/model/grid-configuration.ts
+++ projects/ngx-darkbox-gallery/src/lib/model/grid-configuration.ts
@@ -5,14 +5,14 @@ export interface GridConfiguration {
    * Number of images loaded initially on small and medium sized devices (smartphone / tablet)
    * This is scaled by a factor according to the resolution of bigger displays
    */
-  initialBatchSize: number;
+  initialBatchSize?: number;
   /**
    * The number of images loaded in each load more images call
    */
-  batchSize: number;
+  batchSize?: number;
 
   /**
    * A list of scaling factors to be used to scale the initial batch size by screen width
    */
-  batchSizeScalingFactors: ResolutionConfiguration[];
+  batchSizeScalingFactors?: ResolutionConfiguration[];
 }
