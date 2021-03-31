378b8316c1d731e16fa62042b0ce0e6bb28d0310
Henning Ramberger <kontakt@hramberger.de>
2021-03-31 19:06:12 +0200

    Fixed typo in comment

--- projects/ngx-darkbox-gallery/src/lib/model/darkbox-configuration.ts
+++ projects/ngx-darkbox-gallery/src/lib/model/darkbox-configuration.ts
@@ -1,14 +1,14 @@
 export interface DarkboxConfiguration {
   /**
    * Sets the direction the darkbox will allow looping through the images
-   * FORWARD: Only after the last image the first will be displayed again
-   * BACKWARD: Only before the first image the last will be displayed again
-   * BOTH: The first image will be displayed after the last and vise versa
+   * FORWARD: Only after the last image the first one will be displayed again
+   * BACKWARD: Only before the first image the last one will be displayed again
+   * BOTH: The first image will be displayed after the last one and vise versa
    * NONE: There is a fixed end in both directions
    */
   loopDirection?: LoopDirection;
   /**
-   * Sets if the caption should be displayed ore not
+   * Sets if the caption should be displayed or not
    * true: Show the caption including the current position in the image set
    * false: Caption is not shown
    */
