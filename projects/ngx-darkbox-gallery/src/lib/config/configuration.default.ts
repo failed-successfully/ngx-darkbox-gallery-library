695983e2995aac35ddd7e23397723d3e117df605
Björn Böhnke <git@bjoern-boehnke.de>
2021-04-25 16:45:49 +0200

    Apply suggestions from code review
    Co-authored-by: Henning Ramberger <github@hramberger.de>

--- projects/ngx-darkbox-gallery/src/lib/config/configuration.default.ts
+++ projects/ngx-darkbox-gallery/src/lib/config/configuration.default.ts
@@ -1,6 +1,6 @@
-import { Configuration } from "../model/configuration";
-import { DefaultDarkboxConfiguration } from "./darkbox-configuration.default";
-import { DefaultGridConfiguration } from "./grid-configuration.default";
+import { Configuration } from '../model/configuration';
+import { DefaultDarkboxConfiguration } from './darkbox-configuration.default';
+import { DefaultGridConfiguration } from './grid-configuration.default';
 import { DefaultImageConfiguration } from './image-configuration.default';
 
 export class DefaultConfiguration implements Configuration {
