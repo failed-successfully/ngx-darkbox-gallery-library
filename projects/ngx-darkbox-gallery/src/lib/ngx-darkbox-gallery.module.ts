d36c3949bb1e431e59efd16b3623a1e153fce7fc
Henning Ramberger <kontakt@hramberger.de>
2021-04-26 18:51:17 +0200

    Added first draft of the loading placeholder

--- projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.module.ts
+++ projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxDarkboxGalleryComponent } from './ngx-darkbox-gallery.component';
 import { DarkboxComponent } from './components/darkbox/darkbox.component';
+import { LoadingPlaceholderComponent } from './components/loading-placeholder/loading-placeholder.component';
 
 @NgModule({
   declarations: [
     NgxDarkboxGalleryComponent,
-    DarkboxComponent
+    DarkboxComponent,
+    LoadingPlaceholderComponent
   ],
   imports: [
     BrowserModule,
