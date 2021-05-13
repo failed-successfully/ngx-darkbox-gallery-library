4f129ab3d5da110809434797a16cb91964b464a9
Henning Ramberger <kontakt@hramberger.de>
2021-05-13 17:20:49 +0200

    Moved loading animations to a subfolder

--- projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.module.ts
+++ projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DarkboxComponent } from './components/darkbox/darkbox.component';
 import { LoadingPlaceholderComponent } from './components/loading-placeholder/loading-placeholder.component';
-import { DotComponent } from './components/loading/dot/dot.component';
-import { FlexRingComponent } from './components/loading/flex-ring/flex-ring.component';
-import { SvgLoaderComponent } from './components/loading/svg-loader/svg-loader.component';
+import { DotComponent } from './components/loading/loading-animation/dot/dot.component';
+import { FlexRingComponent } from './components/loading/loading-animation/flex-ring/flex-ring.component';
+import { SvgLoaderComponent } from './components/loading/loading-animation/svg-loader/svg-loader.component';
 import { NgxDarkboxGalleryComponent } from './ngx-darkbox-gallery.component';
 
 @NgModule({
