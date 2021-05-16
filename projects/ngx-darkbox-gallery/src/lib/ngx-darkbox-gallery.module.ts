b44913bdf33653bbda2161e74c3b6d1e9f9b7ef6
Henning Ramberger <kontakt@hramberger.de>
2021-05-16 18:10:25 +0200

    Moved loading placeholder to loding directory

--- projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.module.ts
+++ projects/ngx-darkbox-gallery/src/lib/ngx-darkbox-gallery.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DarkboxComponent } from './components/darkbox/darkbox.component';
-import { LoadingPlaceholderComponent } from './components/loading-placeholder/loading-placeholder.component';
+import { LoadingPlaceholderComponent } from './components/loading/loading-placeholder/loading-placeholder.component';
 import { DotComponent } from './components/loading/loading-animation/dot/dot.component';
 import { FlexRingComponent } from './components/loading/loading-animation/flex-ring/flex-ring.component';
 import { SvgLoaderComponent } from './components/loading/loading-animation/svg-loader/svg-loader.component';
