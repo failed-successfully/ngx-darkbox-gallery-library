b76b588c5482fd4d5e5660c5628c8d36ee94cf23
Henning Ramberger <kontakt@hramberger.de>
2021-05-16 20:14:58 +0200

    Added switch to toggle the dark mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgxDarkboxGalleryModule } from '@failed-successfully/ngx-darkbox-gallery';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { NgxDarkboxGalleryModule } from '@failed-successfully/ngx-darkbox-galler
   ],
   imports: [
     BrowserModule,
-    NgxDarkboxGalleryModule
+    NgxDarkboxGalleryModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
