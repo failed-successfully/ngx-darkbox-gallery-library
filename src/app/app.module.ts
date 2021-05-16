af3ea4beb58813b08f7de4599f497119233933ea
Henning Ramberger <kontakt@hramberger.de>
2021-05-16 20:54:21 +0200

    Moved dark mode switch to its on component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgxDarkboxGalleryModule } from '@failed-successfully/ngx-darkbox-gallery';
 import { FormsModule } from '@angular/forms';
+import { DarkModeSwitchComponent } from './dark-mode-switch/dark-mode-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DarkModeSwitchComponent
   ],
   imports: [
     BrowserModule,
