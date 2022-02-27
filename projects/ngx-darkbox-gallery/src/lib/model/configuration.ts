aae58520cb9f926fff8edeb13a7416134d16ad85
bjoernboehnke <git@bjoern-boehnke.de>
2022-02-27 17:12:58 +0100

    refactor(config): removed unused image-configuration

--- projects/ngx-darkbox-gallery/src/lib/model/configuration.ts
+++ projects/ngx-darkbox-gallery/src/lib/model/configuration.ts
@@ -1,9 +1,7 @@
 import { DarkboxConfiguration } from "./darkbox-configuration";
 import { GridConfiguration } from "./grid-configuration";
-import { ImageConfiguration } from "./image-configuration";
 
 export interface Configuration {
-  imageConfiguration?: ImageConfiguration;
   /**
    * Sets the look and features of the lighbox component of Darkbox
    */
--- projects/ngx-darkbox-gallery/src/public-api.ts
+++ projects/ngx-darkbox-gallery/src/public-api.ts
@@ -9,6 +9,5 @@ export * from './lib/model/color-configuration';
 export * from './lib/model/configuration';
 export * from './lib/model/darkbox-configuration';
 export * from './lib/model/grid-configuration';
-export * from './lib/model/image-configuration';
 export * from './lib/model/image';
 export * from './lib/model/resolution-configuration';
