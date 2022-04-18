8112f1e74ea4a4699a8a314a6b371c1085b8daa7
Henning Ramberger <kontakt@hramberger.de>
2022-04-18 20:25:48 +0200

    feat(demo): Add consistent color picker to demo (#424)
    * feat(demo): Add consistent color picker to demo
    
    * docs(changelog): Add changelog entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxDarkboxGalleryModule } from '@failed-successfully/ngx-darkbox-gallery';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { ConfigurationEditorComponent } from './components/configuration-editor/configuration-editor.component';
 import { DarkModeSwitchComponent } from './components/dark-mode-switch/dark-mode-switch.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxJsonViewerModule } from 'ngx-json-viewer';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { ToastrModule } from 'ngx-toastr';
     MatSelectModule,
     MatInputModule,
     ToastrModule.forRoot(),
+    ColorPickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
