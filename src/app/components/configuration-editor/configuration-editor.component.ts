e8d26ceb73e6f8db3e576ed10fc09cb8ae6b7d9c
Henning Ramberger <kontakt@hramberger.de>
2025-06-08 16:39:33 +0200

    chore: Update dependencies

--- src/app/components/configuration-editor/configuration-editor.component.ts
+++ src/app/components/configuration-editor/configuration-editor.component.ts
@@ -1,23 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Configuration, ButtonStyle, GridType, LoadingAnimation, LoopDirection, ResolutionConfiguration } from 'projects/ngx-darkbox-gallery/src/public-api';
-import { ToastrService } from 'ngx-toastr';
-import { MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle } from '@angular/material/expansion';
-import { MatSelect } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
-import { MatOption } from '@angular/material/core';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatCheckbox } from '@angular/material/checkbox';
-import { ColorPickerModule } from 'ngx-color-picker';
+import { MatOption } from '@angular/material/core';
+import { MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle } from '@angular/material/expansion';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { MatIconButton, MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { MatSelect } from '@angular/material/select';
+import { ColorPickerComponent, ColorPickerDirective } from 'ngx-color-picker';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import { ToastrService } from 'ngx-toastr';
+import { ButtonStyle, Configuration, GridType, LoadingAnimation, LoopDirection, ResolutionConfiguration } from 'projects/ngx-darkbox-gallery/src/public-api';
 
 @Component({
     selector: 'app-configuration-editor',
     templateUrl: './configuration-editor.component.html',
     styleUrls: ['./configuration-editor.component.scss'],
-    imports: [MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, MatSelect, FormsModule, MatOption, MatCheckbox, ColorPickerModule, MatFormField, MatLabel, MatInput, MatIconButton, MatIcon, MatButton, NgxJsonViewerModule]
+    imports: [MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, MatSelect, FormsModule, MatOption, MatCheckbox, ColorPickerDirective, MatFormField, MatLabel, MatInput, MatIconButton, MatIcon, MatButton, NgxJsonViewerModule]
 })
 export class ConfigurationEditorComponent {
 
