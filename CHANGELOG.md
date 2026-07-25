0f9672906527892a3bf93e8de639def4e7036a1e
Henning Ramberger <kontakt@hramberger.de>
2026-07-25 21:15:03 +0200

    chore: Convert image class to an interface

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 * Remove deprecated keyboard navigation binding
+* Convert image class to aninterface
 
 ## [5.0.1] - 2026-05-13
 
--- projects/ngx-darkbox-gallery/src/lib/model/image.ts
+++ projects/ngx-darkbox-gallery/src/lib/model/image.ts
@@ -1,22 +1,22 @@
-export class Image {
+export interface Image {
   /**
    * An id to identify the image in events. This is not used by the library itself
    */
-  public identifier?: string;
+  identifier?: string;
   /**
    * URL of the full sized image
    */
-  public url: string;
+  url: string;
   /**
    * URL of the thumbnail image
    */
-  public thumbnailUrl: string;
+  thumbnailUrl: string;
   /**
    * A short caption that can be shown under the  image in darkbox
    */
-  public caption?: string;
+  caption?: string;
   /**
    * The alternative text shown when the image can not be loaded
    */
-  public altText?: string;
+  altText?: string;
 }
